41dd4cd727d4c0d0ff40da20599081ecd479b1bf
RenaarsA <DogieLV@Gmail.com>
2023-02-10 01:55:50 +1345

    Update search.py

--- search.py
+++ search.py
@@ -1,22 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
 
-def find_movie_streaming_sites(movie_name):
-    query = movie_name + " watch online"
+def find_movie_on_soap2day(movie_name):
+    query = movie_name + " site:soap2day.to"
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
-    google_search = requests.get(f"https://www.google.com/search?q={query}", headers=headers)
-    soup = BeautifulSoup(google_search.text, "html.parser")
-    links = [link.get("href") for link in soup.find_all("a")]
-    streaming_sites = []
-    for link in links:
-        if "watch" in link and "online" in link:
-            if link.startswith("http"):
-                streaming_sites.append(link)
-    return streaming_sites
+    soap2day_search = requests.get(f"https://www.google.com/search?q={query}", headers=headers)
+    soup = BeautifulSoup(soap2day_search.text, "html.parser")
+    movie_link = soup.find("a", href=lambda x: "soap2day.to" in x)
+    if movie_link:
+        return movie_link["href"]
+    else:
+        return None
 
 movie_name = input("Enter movie name: ")
-sites = find_movie_streaming_sites(movie_name)
-print("Sites where you can watch the movie:")
-for site in sites:
-    print(site)
+link = find_movie_on_soap2day(movie_name)
+if link:
+    print(f"Link to the movie '{movie_name}' on soap2day: {link}")
+else:
+    print(f"Movie '{movie_name}' not found on soap2day.")
